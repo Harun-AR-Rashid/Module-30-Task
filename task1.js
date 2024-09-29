df03f9786ba66df0c5867d96b147aceebc3432f6
Harun-AR-Rashid <mdharunarrashid1998@gmail.com>
2024-09-29 12:06:05 +0600

    problem one done

--- task1.js
+++ task1.js
@@ -1,1 +1,13 @@
-console.log('I love you Halima')
+// Problem-1
+const arrowFunction = (x, y, z) => x * y * z;
+// console.log(arrowFunction(2, 3, 4));
+// task-2
+const result = `
+I am a Web Developer.
+I Love to code.
+I Love to eat biryani.
+`;
+// console.log(result);
+// task-3
+const total = (num, curr=7) => num + curr;
+// console.log(total(8));
